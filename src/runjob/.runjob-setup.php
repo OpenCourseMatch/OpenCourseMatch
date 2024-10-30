f1bca26e24f8217929bb749d8b83facfc0507628
JensOstertag <dev@jens-ostertag.de>
2024-10-30 11:04:43 +0100

    Allocation algorithm structure

---
+++ src/runjob/.runjob-setup.php
@@ -0,0 +1,7 @@
+<?php
+
+// Application directory: The directory where the application is located in the filesystem
+chdir("../../");
+const __APP_DIR__ = __DIR__ . "/../..";
+
+require_once(__APP_DIR__ . "/framework/framework.php");
---
+++ src/runjob/allocation-algorithm.php
@@ -0,0 +1,5 @@
+<?php
+
+require __DIR__ . "/.runjob-setup.php";
+
+Logger::getLogger("AllocationAlgorithm")->info("Starting allocation algorithm");
