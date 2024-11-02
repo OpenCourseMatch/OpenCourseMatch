fb6c96487e1eb1bf07e934e8d31ce7309a6328bd
JensOstertag <dev@jens-ostertag.de>
2024-11-02 17:57:29 +0100

    Continue algorithm runjob

--- src/runjob/allocation-algorithm.php
+++ src/runjob/allocation-algorithm.php
@@ -3,3 +3,8 @@
 require __DIR__ . "/.runjob-setup.php";
 
 Logger::getLogger("AllocationAlgorithm")->info("Starting allocation algorithm");
+
+$algorithm = new AllocationAlgorithm();
+$algorithm->run();
+
+Logger::getLogger("AllocationAlgorithm")->info("Allocation algorithm finished");
