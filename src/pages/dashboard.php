5e271074d0f616988c4967bb310060d2a36f866c
JensOstertag <dev@jens-ostertag.de>
2024-11-26 23:48:20 +0100

    Show warning in dashboard when assignment algorithm is running

--- src/pages/dashboard.php
+++ src/pages/dashboard.php
@@ -2,4 +2,10 @@
 
 $user = Auth::enforceLogin(PermissionLevel::USER->value, Router::generate("index"));
 
+if($user->getPermissionLevel() === PermissionLevel::ADMIN->value) {
+    if(SystemStatus::dao()->get("algorithmRunning") === "true") {
+        new InfoMessage(t("The course assignment algorithm is currently running. Meanwhile, some actions from your dashboard might be unavailable."), InfoMessageType::WARNING);
+    }
+}
+
 echo Blade->run("dashboard");
