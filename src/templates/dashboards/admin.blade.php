57c557004a137dfff492d16c78c7b55634d817c6
JensOstertag <dev@jens-ostertag.de>
2024-12-03 11:03:34 +0100

    Replace include directives with component directive

--- src/templates/dashboards/admin.blade.php
+++ src/templates/dashboards/admin.blade.php
@@ -13,47 +13,42 @@
     {{ t("Manage accounts") }}
 </h2>
 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-    @include("components.dashboardlink", [
+    @component("components.dashboardlink", [
         "icon" => "components.icons.group",
         "href" => Router::generate("groups-overview"),
         "title" => t("Groups"),
-        "description" => t("Customize user groups to model the participation requirements of the courses."),
-        "danger" => false
-    ])
-    @include("components.dashboardlink", [
+        "description" => t("Customize user groups to model the participation requirements of the courses.")
+    ])@endcomponent
+    @component("components.dashboardlink", [
         "icon" => "components.icons.user",
         "href" => Router::generate("users-overview"),
         "title" => t("Participants and tutors"),
-        "description" => t("Manage accounts of participants and tutors."),
-        "danger" => false
-    ])
-    @include("components.dashboardlink", [
+        "description" => t("Manage accounts of participants and tutors.")
+    ])@endcomponent
+    @component("components.dashboardlink", [
         "icon" => "components.icons.user",
         "href" => Router::generate("facilitators-overview"),
         "title" => t("Facilitators"),
-        "description" => t("Manage accounts of facilitators."),
-        "danger" => false
-    ])
-    @include("components.dashboardlink", [
+        "description" => t("Manage accounts of facilitators.")
+    ])@endcomponent
+    @component("components.dashboardlink", [
         "icon" => "components.icons.user",
         "href" => Router::generate("administrators-overview"),
         "title" => t("Administrators"),
-        "description" => t("Manage accounts of administrators."),
-        "danger" => false
-    ])
+        "description" => t("Manage accounts of administrators.")
+    ])@endcomponent
 </div>
 
 <h2 class="mt-4 mb-2">
     {{ t("Manage courses") }}
 </h2>
 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-    @include("components.dashboardlink", [
+    @component("components.dashboardlink", [
         "icon" => "components.icons.course",
         "href" => Router::generate("courses-overview"),
         "title" => t("Courses"),
-        "description" => t("Manage the available courses."),
-        "danger" => false
-    ])
+        "description" => t("Manage the available courses.")
+    ])@endcomponent
 </div>
 
 <h2 class="mt-4 mb-2">
@@ -61,24 +56,23 @@
 </h2>
 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
     @if(SystemStatus::dao()->get("coursesAssigned") !== "true" && SystemStatus::dao()->get("algorithmRunning") !== "true")
-        @include("components.dashboardlink", [
+        @component("components.dashboardlink", [
             "icon" => "components.icons.algorithm",
             "href" => Router::generate("course-assignment-run"),
             "title" => t("Run course assignment"),
-            "description" => t("Start the assignment algorithm to group participants to the courses based on their preferences."),
-            "danger" => false
-        ])
+            "description" => t("Start the assignment algorithm to group participants to the courses based on their preferences.")
+        ])@endcomponent
     @endif
     {{-- Edit course assignment --}}
     {{-- Print course assignment --}}
     @if(SystemStatus::dao()->get("coursesAssigned") === "true")
-        @include("components.dashboardlink", [
+        @component("components.dashboardlink", [
             "icon" => "components.icons.reset",
             "href" => Router::generate("course-assignment-reset"),
             "title" => t("Reset course assignment"),
             "description" => t("Reset the course assignment to re-run the assignment algorithm."),
             "danger" => true
-        ])
+        ])@endcomponent
     @endif
 </div>
 
@@ -86,13 +80,12 @@
     {{ t("Settings") }}
 </h2>
 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-    @include("components.dashboardlink", [
+    @component("components.dashboardlink", [
         "icon" => "components.icons.gear",
         "href" => Router::generate("system-settings"),
         "title" => t("System settings"),
-        "description" => t("Configure OpenCourseMatch to your organizations' needs."),
-        "danger" => false
-    ])
+        "description" => t("Configure OpenCourseMatch to your organizations' needs.")
+    ])@endcomponent
     {{-- Reset all data --}}
 </div>
 
@@ -100,13 +93,12 @@
     {{ t("About OpenCourseMatch") }}
 </h2>
 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-    @include("components.dashboardlink", [
+    @component("components.dashboardlink", [
         "icon" => "components.icons.bug",
         "href" => "https://github.com/OpenCourseMatch/OpenCourseMatch/issues/new/choose",
         "title" => t("Bug reports and feature requests"),
         "description" => t("Found a bug or have an idea to improve OpenCourseMatch? Please create an issue in our GitHub repository."),
-        "external" => true,
-        "danger" => false
-    ])
+        "external" => true
+    ])@endcomponent
     {{-- Changelog --}}
 </div>
