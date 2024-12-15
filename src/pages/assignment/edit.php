437785a35f2f58afab2a0bb9e9c69c08c0238812
JensOstertag <dev@jens-ostertag.de>
2024-12-15 14:41:39 +0100

    Edit assignment script

---
+++ src/pages/assignment/edit.php
@@ -0,0 +1,11 @@
+<?php
+
+$user = Auth::enforceLogin(PermissionLevel::ADMIN->value, Router::generate("index"));
+$coursesAssigned = SystemStatus::dao()->get("coursesAssigned") === "true";
+
+if(!$coursesAssigned) {
+    new InfoMessage(t("An error has occurred whilst attempting to edit the course assignment. Please try again later."), InfoMessageType::ERROR);
+    Comm::redirect(Router::generate("index"));
+}
+
+echo Blade->run("assignment.edit");
