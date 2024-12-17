21aa2bae90c9acab86fdd5b775de92420a074c39
JensOstertag <dev@jens-ostertag.de>
2024-12-17 22:24:26 +0100

    Try around with course overview design

--- src/templates/assignment/edit.blade.php
+++ src/templates/assignment/edit.blade.php
@@ -6,7 +6,7 @@
         {{ t("Edit course assignment") }}
     </h1>
 
-    <div class="flex justify-between">
+    <div class="flex justify-between mb-2">
         <button class="{{ TailwindUtil::button() }} mb-2 gap-2">
             @include("components.icons.left")
             {{ t("Previous course") }}
@@ -16,5 +16,18 @@
             {{ t("Next course") }}
             @include("components.icons.right")
         </button>
+    </div>
+
+    <div data-courseoverview="">
+        <div class="flex flex-col md:flex-row">
+            <h2>Course title</h2>
+            <span class="">
+                0 / 0 / 0 <!--Min / Current / Max -->
+            </span>
+            <span class="">
+                0 - 0 <!-- Clearance level -->
+            </span>
+        </div>
+        <!-- TODO: Error messages -->
     </div>
 @endcomponent
