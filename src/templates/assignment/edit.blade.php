8da7ef59ec2538b4c40215acd5b3f0f175de824c
JensOstertag <dev@jens-ostertag.de>
2024-12-18 10:38:09 +0100

    Scoped labels design

--- src/templates/assignment/edit.blade.php
+++ src/templates/assignment/edit.blade.php
@@ -6,7 +6,7 @@
         {{ t("Edit course assignment") }}
     </h1>
 
-    <div class="flex justify-between mb-2">
+    <div class="flex justify-between gap-2 mb-2">
         <button class="{{ TailwindUtil::button() }} mb-2 gap-2">
             @include("components.icons.left")
             {{ t("Previous course") }}
@@ -19,14 +19,26 @@
     </div>
 
     <div data-courseoverview="">
-        <div class="flex flex-col md:flex-row">
+        <div class="flex flex-col sm:flex-row items-center gap-x-4 gap-y-2 flex-wrap">
             <h2>Course title</h2>
-            <span class="">
-                0 / 0 / 0 <!--Min / Current / Max -->
-            </span>
-            <span class="">
-                0 - 0 <!-- Clearance level -->
-            </span>
+            <div class="flex flex-wrap gap-2">
+                <div class="flex flex-row whitespace-nowrap">
+                    <span class="pl-2 pr-1 text-primary-font bg-primary rounded-l-full border border-primary">
+                        Participants
+                    </span>
+                    <span class="pl-1 pr-2 rounded-r-full border border-primary">
+                        0 / 0 / 0
+                    </span>
+                </div>
+                <div class="flex flex-row whitespace-nowrap">
+                    <span class="pl-2 pr-1 text-primary-font bg-primary rounded-l-full border border-primary">
+                        Clearance level
+                    </span>
+                    <span class="pl-1 pr-2 rounded-r-full border border-primary">
+                        0 - 0
+                    </span>
+                </div>
+            </div>
         </div>
         <!-- TODO: Error messages -->
     </div>
