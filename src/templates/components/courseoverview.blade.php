3ef161b352949ebde4fe47f8eab0bf572b39d47e
JensOstertag <dev@jens-ostertag.de>
2024-12-22 13:36:10 +0100

    Fix line indentations

--- src/templates/components/courseoverview.blade.php
+++ src/templates/components/courseoverview.blade.php
@@ -5,26 +5,26 @@
             @if($course !== null)
                 {{ $course->getTitle() }}
             @else
-                {{ t("Unallocated users") }}
+                {{ t("Unassigned users") }}
             @endif
         </h2>
         @if($course !== null)
             <div class="flex flex-wrap gap-2">
                 <div class="flex flex-row whitespace-nowrap">
-                            <span class="pl-2 pr-1 text-primary-font bg-primary rounded-l-full border border-primary">
-                                {{ t("Participants") }}
-                            </span>
+                    <span class="pl-2 pr-1 text-primary-font bg-primary rounded-l-full border border-primary">
+                        {{ t("Participants") }}
+                    </span>
                     <span class="pl-1 pr-2 rounded-r-full border border-primary">
-                                {{ $course->getMinParticipants() }} / {{ $realParticipantCount }} / {{ $course->getMaxParticipants() }}
-                            </span>
+                        {{ $course->getMinParticipants() }} / {{ $realParticipantCount }} / {{ $course->getMaxParticipants() }}
+                    </span>
                 </div>
                 <div class="flex flex-row whitespace-nowrap">
-                            <span class="pl-2 pr-1 text-primary-font bg-primary rounded-l-full border border-primary">
-                                {{ t("Clearance level") }}
-                            </span>
+                    <span class="pl-2 pr-1 text-primary-font bg-primary rounded-l-full border border-primary">
+                        {{ t("Clearance level") }}
+                    </span>
                     <span class="pl-1 pr-2 rounded-r-full border border-primary">
-                                {{ $course->getMinClearance() }} - {{ $course->getMaxClearance() }}
-                            </span>
+                        {{ $course->getMinClearance() }} - {{ $course->getMaxClearance() }}
+                    </span>
                 </div>
             </div>
         @endif
@@ -34,23 +34,23 @@
     @if($course !== null)
         @if($course->isCancelled())
             @component("components.layout.infomessage", [
-            "type" => InfoMessageType::WARNING
-        ])
+                "type" => InfoMessageType::WARNING
+            ])
                 {{ t("This course has been cancelled.") }}
             @endcomponent
         @else
             @if($course->getMaxParticipants() < $realParticipantCount)
                 @component("components.layout.infomessage", [
-                "type" => InfoMessageType::WARNING
-            ])
+                    "type" => InfoMessageType::WARNING
+                ])
                     {{ t("The number of participants exceeds the maximum number of participants allowed for this course.") }}
                 @endcomponent
             @endif
 
             @if($course->getMinParticipants() > $realParticipantCount)
                 @component("components.layout.infomessage", [
-                "type" => InfoMessageType::WARNING
-            ])
+                    "type" => InfoMessageType::WARNING
+                ])
                     {{ t("The number of participants is below the minimum number of participants required for this course.") }}
                 @endcomponent
             @endif
