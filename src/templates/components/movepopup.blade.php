4c4d86b4edeefbab210f7c45cf264826d8773591
JensOstertag <dev@jens-ostertag.de>
2025-01-15 11:11:48 +0100

    Margins in move popup

--- src/templates/components/movepopup.blade.php
+++ src/templates/components/movepopup.blade.php
@@ -1,10 +1,10 @@
 <div>
     @if($leadingCourse !== null)
-        <h3>
+        <h3 class="mb-2">
             {{ t("Leading course") }}
         </h3>
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mb-4">
             @include("components.movecourse", [
                 "course" => $leadingCourse,
                 "errors" => $errors,
@@ -14,11 +14,11 @@
     @endif
 
     @if(!empty($chosenCourses))
-        <h3>
+        <h3 class="mb-2">
             {{ t("Chosen courses") }}
         </h3>
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mb-4">
             @foreach($chosenCourses as $course)
                 @include("components.movecourse", [
                     "course" => $course,
@@ -30,11 +30,11 @@
     @endif
 
     @if(!empty($otherCourses))
-        <h3>
+        <h3 class="mb-2">
             {{ t("Other courses") }}
         </h3>
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mb-4">
             @foreach($otherCourses as $course)
                 @include("components.movecourse", [
                     "course" => $course,
