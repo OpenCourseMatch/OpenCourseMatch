b9d282288b389bf012523fc23e2a5b6f876c99b7
JensOstertag <dev@jens-ostertag.de>
2025-02-27 18:24:15 +0100

    Base chart initialization

---
+++ src/static/js/statistics/overview.js
@@ -0,0 +1,58 @@
+const initChart = (id, title, type, dataLabel, data, labels, fontFamily, baseOptions) => {
+    let canvas = document.getElementById(id).getContext("2d");
+
+    let chartData;
+    if(Array.isArray(dataLabel)) {
+        chartData = {
+            labels: labels,
+            datasets: []
+        };
+        data.forEach((dataset, index) => {
+            chartData.datasets.push({
+                label: dataLabel[index],
+                data: dataset
+            });
+        });
+    } else {
+        chartData = {
+            labels: labels,
+            datasets: [{
+                label: dataLabel,
+                data: data
+            }]
+        };
+    }
+
+    baseOptions = baseOptions || {};
+
+    return new Chart(canvas, {
+        type: type,
+        data: chartData,
+        options: {
+            ...baseOptions,
+            responsive: true,
+            aspectRatio: 1,
+            maintainAspectRatio: false,
+            plugins: {
+                title: {
+                    display: true,
+                    text: title,
+                    font: {
+                        family: fontFamily || "OpenSans"
+                    }
+                },
+                legend: {
+                    display: true,
+                    position: "bottom",
+                    labels: {
+                        font: {
+                            family: fontFamily || "OpenSans"
+                        }
+                    }
+                }
+            }
+        }
+    });
+}
+
+export default {  };
