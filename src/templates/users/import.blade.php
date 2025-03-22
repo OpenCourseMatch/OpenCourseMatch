96f6a335addd24427d4b63235211bda558fdca59
JensOstertag <dev@jens-ostertag.de>
2025-03-22 09:55:57 +0100

    Import description

--- src/templates/users/import.blade.php
+++ src/templates/users/import.blade.php
@@ -6,6 +6,31 @@
         {{ t("Import users") }}
     </h1>
 
+    <p>
+        {{ t("You can create accounts for a list of users all at once by importing the data from a CSV file.") }}
+        {{ t("It has to follow this format:") }}
+    </p>
+    <div class="flex justify-center my-2">
+        <table>
+            <thead class="bg-primary text-primary-font">
+                <tr>
+                    <th class="p-2 border border-primary">A</th>
+                    <th class="p-2 border border-primary">B</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="p-2 border border-gray">{{ t("Last name") }}</td>
+                    <td class="p-2 border border-gray">{{ t("First name") }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <p>
+        {{ t("The columns must be separated by a comma, and the first line is not ignored.") }}
+        {{ t("The import is limited to 50 rows at a time.") }}
+    </p>
+
     <form method="post" action="{{ Router::generate("users-import-action") }}" data-redirect="{{ Router::generate("users-overview") }}">
         <div class="{{ TailwindUtil::inputGroup() }} mb-2">
             <label for="file" class="{{ TailwindUtil::$inputLabel }}" data-required>
@@ -16,7 +41,7 @@
                    type="file"
                    class="{{ TailwindUtil::$input }}"
                    placeholder="{{ t("Choose file") }}"
-                   {{-- TODO: File type and size --}}
+                   accept="text/csv"
                    required>
         </div>
 
